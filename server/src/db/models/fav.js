d99478852508121b0d6eab6356bcc9534206c113
«aselya93» <asel_hbk@mail.ru>
2024-12-04 13:41:38 +0300

    dbMigrate

---
+++ server/src/db/models/fav.js
@@ -0,0 +1,20 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Fav extends Model {
+
+    static associate(models) {
+      // define association here
+    }
+  }
+  Fav.init({
+    userId: DataTypes.INTEGER,
+    recipeId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Fav',
+  });
+  return Fav;
+};
