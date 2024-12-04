c8a0c5597d9faa7aaefd9cefd35f10f775115ffc
«aselya93» <asel_hbk@mail.ru>
2024-12-04 18:14:41 +0300

    Server-Fav

--- server/src/routes/index.routes.js
+++ server/src/routes/index.routes.js
@@ -2,10 +2,13 @@ const router = require("express").Router();
 const recipeRoutes = require("./recipe.routes");
 const authRoutes = require("./auth.routes");
 const formatResponse = require("../utils/formatResponse");
+const favRoutes = require('./fav.routes')
 
 router
 .use("/recipes", recipeRoutes)
-.use("/auth", authRoutes);
+.use("/auth", authRoutes)
+.use('/fav', favRoutes)
+
 
 router.use("*", (req, res) => {
   res
--- server/src/services/Fav.service.js
+++ server/src/services/Fav.service.js
@@ -14,16 +14,13 @@ class FavService {
         })
     }
     static async createFav(userId, recipeId) {
-        const newFav = await Fav.create({
-               userId: userId,
-             recipeId: recipeId,
-        })
+        const newFav = await Fav.create({ userId, recipeId })
         return newFav
     }
     static async deleteFav(userId, recipeId) {
-        const deleteFavCount = await this.getByIdFav(userId, recipeId)
+        const deleteFavCount = await this.getFav(userId, recipeId)
         if(deleteFavCount) {
-            await deleteFavCount.destroy()
+            await deleteFavCount.destroy({ where: { userId, recipeId }})
         }
         return deleteFavCount
     }
