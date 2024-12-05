bb3f0165ca07a0852cda43854f5a56d891a26708
MariaKh28 <khrenovamar@gmail.com>
2024-12-05 14:45:58 +0300

    addToFav/deleteFromFav

--- client/src/widgets/Navigation/RecipeForm/RecipeForm.jsx
+++ client/src/widgets/Navigation/RecipeForm/RecipeForm.jsx
@@ -1,13 +1,15 @@
 export default function RecipeForm({ input, setInput}) {
+
   return (
     <div>
-      <h1>Поиск рецептов</h1>
+        <h1>Поиск рецептов</h1>
      <input
                 type="text"
                 value={input}
                 onChange={(e) => setInput(e.target.value)}
                 placeholder="Введите название блюда"
     />
+      
     </div>
   );
 }
